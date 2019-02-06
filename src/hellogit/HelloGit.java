f9557737cb51e610d4a20a70afdf770c02aa4619
m3fog <m3fog@134.124.24.201>
2019-02-05 18:15:54 -0600

    Added

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -14,8 +14,10 @@ public class HelloGit {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, Git!");
+
     }
     
 }
